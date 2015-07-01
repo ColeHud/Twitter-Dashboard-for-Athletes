9863d1b4bc31af38edf2549241425c09d3a47dd2
ColeHud <cmhudson11@gmail.com>
2015-07-01 17:12:18 -0400

    Started project
    Basic UI. Started adding libraries.

---
+++ Twitter Dashboard for Athletes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Twitter Dashboard for Athletes
+//
+//  Created by Cole on 7/1/15.
+//  Copyright (c) 2015 Cole Hudson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Twitter Dashboard for Athletes/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  
+//
+//  Created by Cole on 7/1/15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
